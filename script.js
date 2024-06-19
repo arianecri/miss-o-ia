7fbbf1ce12c62ca269ec133fcb9a6d739b864492
ARIANE CRISTINA ALVES <111357510+arianecri@users.noreply.github.com>
2024-06-19 14:53:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const caixa-principal= document.querySelector("caixa-principal");
+const caixa-PERGUNTA= document.querySelector("caixa-PERGUNTA");
+const caixa-alternativa= document.querySelector("caixa-alternativa);
+const caixa-resultado= document.querySelector("caixa-resultado);
+const texto-resultado= document.querySelector("texto-resultado);
+
+const pergunta1=[Pergunta 1,"Pergunta 2"]
